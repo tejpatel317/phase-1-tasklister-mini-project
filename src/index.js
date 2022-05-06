a0b35e0d3e2167b414f5f0d496868c7348bf8c7e
Tej Patel <tejpatel317@gmail.com>
2022-05-06 16:17:29 -0400

    completed project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let userInput = (e.target.querySelector("#new-task-description").value)
+    buildTodos(userInput)
+    form.reset()
+  });
 });
+
+function buildTodos(userInput) {
+  let li = document.createElement("li")
+  let btn = document.createElement("button")
+  btn.addEventListener('click', handleDelete)
+  li.textContent = `${userInput} `
+  btn.textContent = "X"
+  li.appendChild(btn)
+  let userPriority = document.querySelector("select").value;
+  if (userPriority === "Red") {
+    li.style.color = "Red"
+  }
+  else if (userPriority === "Orange") {
+    li.style.color = "Orange"
+  }
+  else if (userPriority === "Green") {
+    li.style.color = "Green"
+  }
+  document.querySelector("#tasks").appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
